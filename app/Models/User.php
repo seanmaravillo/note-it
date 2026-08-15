cda08d9f1c26a7cca7a67f4743d67c035298960f
Sean Maravillo <fortech.sean@gmail.com>
2026-08-15 09:17:44 +0800

    Added Note model and notes db

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function notes(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Note::class);
+    }
 }
